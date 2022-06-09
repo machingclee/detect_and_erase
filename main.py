d0de91fbf7d203fa8a4e5b2e6828c05a435cafff
Ching C Lee <machingclee@gmail.com>
2022-06-09 21:38:33 +0800

    add test file

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import os
-import sys
 import torch
-
-parent_dir = os.path.normpath( os.path.join(os.path.realpath(__file__), ".."))
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
 
 from chi_text_detection import EAST, compute_boxes
 from chi_text_erasing import InpaintGenerator, resize_and_padding, prefeed_img_transform, reverse_preprocessing
@@ -14,18 +9,16 @@ from src.utils import clean_image
 from src.device import device
 
 
-import torch
-
 def main():
-    east = EAST().to("cpu")
+    east = EAST().to(device)
     east.load_state_dict(
         torch.load("chi_text_detection/final_pth/model_epoch_9.pth", map_location=device)
     )
     east.eval()
 
-    inpaint_gen = InpaintGenerator().to("cpu")
+    inpaint_gen = InpaintGenerator().to(device)
     inpaint_gen.load_state_dict(
-        torch.load("chi_text_erasing/final_pth/model_epoch_2.pth", map_location=device)
+        torch.load("chi_text_erasing/final_pth/model_epoch_6.pth", map_location=device)
     )
     inpaint_gen.eval()
 
@@ -36,9 +29,8 @@ def main():
         basename = os.path.basename(img_path)
         out_path = f"{out_dir}/{basename}"
         img = clean_image(img_path, east, inpaint_gen)
-        img.save(out_path)    
+        img.save(out_path)
         print(f"Saved result in {out_path}.")
-
 
 
 if __name__ == "__main__":
