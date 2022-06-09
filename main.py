6633fc7c2e3ffa4e5e9ddc963390483720546d1b
James Lee <james.lee@raspect.ai>
2022-06-09 19:08:54 +0800

    update detection script

--- main.py
+++ main.py
@@ -1,35 +1,20 @@
+import os
+import sys
+import torch
+
+parent_dir = os.path.normpath( os.path.join(os.path.realpath(__file__), ".."))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
 from chi_text_detection import EAST, compute_boxes
-from chi_text_erasing import InpaintGenerator, preprocessing, prefeed_img_transform, reverse_preprocessing
+from chi_text_erasing import InpaintGenerator, resize_and_padding, prefeed_img_transform, reverse_preprocessing
 from PIL import Image
 from glob import glob
+from src.utils import clean_image
+from src.device import device
+
+
 import torch
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-import torch
-
-
-def clean_image(img_path, text_detection_module, inpaint_module):
-    img = Image.open(img_path)
-    bboxes = compute_boxes(img, text_detection_module, device)
-
-    for bbox in bboxes:
-        """
-        bbox: eight coordinates of 4 points
-        """
-        upper_left = (int(bbox[0]), int(bbox[1])) # x, y
-        box = (bbox[0], bbox[1], bbox[4], bbox[5])
-        txt_img = img.crop(box)
-        txt_img, padding_window, (ori_h, ori_w) = preprocessing(txt_img, return_window=True)
-        txt_img = prefeed_img_transform(txt_img)
-        erasied, _ = inpaint_module(txt_img[None,...])
-        erasied = erasied[0].permute(1, 2, 0).detach().numpy()
-        erasied = reverse_preprocessing(erasied, padding_window, ori_w, ori_h)
-        img.paste(erasied, upper_left)
-    img.show()
-
-
-
 
 def main():
     east = EAST().to("cpu")
@@ -40,18 +25,20 @@ def main():
 
     inpaint_gen = InpaintGenerator().to("cpu")
     inpaint_gen.load_state_dict(
-        torch.load("chi_text_erasing/final_pth/model_epoch_4.pth", map_location=device)
+        torch.load("chi_text_erasing/final_pth/model_epoch_2.pth", map_location=device)
     )
     inpaint_gen.eval()
 
-    test_img_paths = glob("test_img/*.jpg") + glob("test_img/*.png")
+    test_img_paths = glob("test/in/*.jpg") + glob("test_img/in/*.png")
+    out_dir = "test/out"
 
     for img_path in test_img_paths:
-        clean_image(img_path, east, inpaint_gen)
-    
-    
+        basename = os.path.basename(img_path)
+        out_path = f"{out_dir}/{basename}"
+        img = clean_image(img_path, east, inpaint_gen)
+        img.save(out_path)    
+        print(f"Saved result in {out_path}.")
 
-    
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,24 @@
+from chi_text_detection import compute_boxes
+from chi_text_erasing import resize_and_padding, prefeed_img_transform, reverse_preprocessing
+from PIL import Image
+from .device import device
+
+def clean_image(img_path, text_detection_module, inpaint_module):
+    img = Image.open(img_path)
+    bboxes = compute_boxes(img, text_detection_module, device)
+
+    for bbox in bboxes:
+        """
+        bbox: eight coordinates of 4 points
+        """
+        upper_left = (int(bbox[0]), int(bbox[1])) # x, y
+        box = (bbox[0], bbox[1], bbox[4], bbox[5])
+        txt_img = img.crop(box)
+        txt_img, padding_window, original_wh= resize_and_padding(txt_img, return_window=True)
+        txt_img = prefeed_img_transform(txt_img)
+        erasied, _ = inpaint_module(txt_img[None,...])
+        erasied = erasied[0].permute(1, 2, 0).detach().numpy()
+        erasied = reverse_preprocessing(erasied, padding_window, original_wh)
+        img.paste(erasied, upper_left)
+
+    return img
